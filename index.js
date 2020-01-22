cbd70176b0689a2e223918e7f3bc7f811e55ddd5
Zaw Mai <zmai.code@gmail.com>
2020-01-21 16:25:54 -0800

    Added Shipengine api proxy
    Added shipengine api proxy

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const ajaxUrlHost = 'zmai-learn-ajax.glitch.me';
 const shipEngineUrlHost = 'zmai-shipengine-demo.glitch.me';
 
 // proxy middleware options
-var filter = function (pathname, req) {
+var ajaxFilter = function (pathname, req) {
   // replace www.myapp.example with origin(s) that your content will be served from
   // return (req.headers.origin === 'https://www.myapp.example');
   // multiple origin version:
   return (
           (req.headers.origin === 'http://' + ajaxUrlHost) || 
-          (req.headers.origin === 'https://' + ajaxUrlHost) ||
+          (req.headers.origin === 'https://' + ajaxUrlHost)
+         );   
+};
+
+var shipEngineFilter = function (pathname, req) {
+  // replace www.myapp.example with origin(s) that your content will be served from
+  // return (req.headers.origin === 'https://www.myapp.example');
+  // multiple origin version:
+  return (
           (req.headers.origin === 'http://' + shipEngineUrlHost) ||
           (req.headers.origin === 'https://' + shipEngineUrlHost)
          );   
 };
-
+          
 var npsApiOptions = {
   // replace api.datasource.example with the url of your target host
   target: 'https://developer.nps.gov',
@@ -49,14 +57,32 @@ var smartyApiOptions = {
   logLevel: 'debug' // verbose server logging
 };
 
+var shipEngineApiOptions = {
+  // replace api.datasource.example with the url of your target host
+  target: 'https://api.shipengine.com',
+  changeOrigin: true, // needed for virtual hosted sites like Heroku
+  pathRewrite: {
+    '^/shipengine/': '/', // remove endpoint from request path ('^/api/': '/')
+  },
+  onProxyReq: (proxyReq) => {
+    // append key-value pair for API key to end of path
+    // using KEYNAME provided by web service
+    // and KEYVALUE stored in Heroku environment variable
+    proxyReq.path += ('&api_key=' + process.env.NPS_APIKEY);
+  },
+  logLevel: 'debug' // verbose server logging
+};
+
 // create the proxy (without context)
-var npsApiProxy = proxy(filter, npsApiOptions);
-var smartyApiProxy = proxy(filter, smartyApiOptions);
+var npsApiProxy = proxy(ajaxFilter, npsApiOptions);
+var smartyApiProxy = proxy(ajaxFilter, smartyApiOptions);
+var shipEngineApiProxy = proxy(shipEngineFilter, shipEngineApiOptions);
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/nps', npsApiProxy);
 app.use('/smarty', smartyApiProxy);
+app.use('/shipengine', shipEngineApiProxy);
 
 app.listen(app.get('port'));
